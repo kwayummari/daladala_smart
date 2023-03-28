6959e460cf7a87ba35ff288c8216f67fb56533a0
Kwayu Mmari <mmaridenis@gmail.com>
2023-03-28 12:14:48 +0300

    beginninng of the project

---
+++ lib/widgets/button/button.dart
@@ -0,0 +1,38 @@
+import 'package:daladala_smart/widgets/text/text.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+
+class AppButton extends StatelessWidget {
+  final Function onPress;
+  final String label;
+  final Color? bcolor;
+  final double? borderCurve;
+
+  const AppButton(
+      {Key? key,
+      required this.onPress,
+      required this.label,
+      required this.bcolor,
+      required this.borderCurve,
+      })
+      : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        style: ElevatedButton.styleFrom(
+          elevation: 5,
+          foregroundColor: HexColor('#F5841F'),
+          backgroundColor: bcolor,
+          shape: RoundedRectangleBorder(
+              borderRadius: BorderRadius.circular(borderCurve!),
+              side: BorderSide(color: Colors.black)),
+        ),
+        onPressed: () => onPress(),
+        child: AppText(
+          txt: label,
+          color: Colors.white,
+          size: 15,
+        ));
+  }
+}
---
+++ lib/widgets/drawer/app_drawer.dart
@@ -0,0 +1,227 @@
+import 'package:daladala_smart/widgets/text/text.dart';
+import 'package:flutter/material.dart';
+import 'package:hexcolor/hexcolor.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+import 'package:url_launcher/url_launcher.dart';
+
+class AppDrawer extends StatefulWidget {
+  var language;
+  var status;
+  var username;
+  AppDrawer(
+      {Key? key,
+      required this.language,
+      required this.status,
+      required this.username});
+
+  @override
+  State<AppDrawer> createState() => _AppDrawerState();
+}
+
+class _AppDrawerState extends State<AppDrawer> {
+  Future<void> phonecall() async {
+    final Uri launchUri = Uri(
+      scheme: 'tel',
+      path: '0762996305',
+    );
+    await launchUrl(launchUri);
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    double width = MediaQuery.of(context).size.width / 3;
+    return Drawer(
+      backgroundColor: HexColor('#ffffff'),
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Container(
+                color: Colors.white,
+                child: Image.asset(
+                  'assets/full_logo.png',
+                  height: 300,
+                )),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.person_2,
+                color: Colors.black,
+              ),
+              title: AppText(
+                txt: widget.username!,
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              leading: Icon(
+                Icons.newspaper,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: widget.status!,
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.account_box,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Feedback',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.meeting_room,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Appointment',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.meeting_room,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Post Procedures Results',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => phonecall(),
+              leading: Icon(
+                Icons.phone,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Contact us',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.language,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Kiswahili',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.home,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Home',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.settings,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Settings',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            ListTile(
+              onTap: () => null,
+              leading: Icon(
+                Icons.logout,
+                color: Colors.black,
+                size: 15,
+              ),
+              title: AppText(
+                txt: 'Sign Out!',
+                size: 15,
+                color: HexColor('#000000'),
+                weight: FontWeight.w500,
+              ),
+            ),
+            Divider(
+              color: Colors.black,
+            ),
+            SizedBox(
+              height: 40,
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
