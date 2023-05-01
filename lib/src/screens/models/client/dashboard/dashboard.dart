98a7cd897b5244e393ef134dc0a43de36a9047b5
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-01 18:55:05 +0300

    added map-service

--- lib/src/screens/models/client/dashboard/dashboard.dart
+++ lib/src/screens/models/client/dashboard/dashboard.dart
@@ -1,4 +1,5 @@
 import 'package:daladala_smart/src/utils/app_const.dart';
+import 'package:daladala_smart/src/widgets/app-map.dart';
 import 'package:daladala_smart/src/widgets/app_base_screen.dart';
 import 'package:daladala_smart/src/widgets/app_input_text.dart';
 import 'package:flutter/material.dart';
