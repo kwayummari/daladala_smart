d30cbdbff933c2c7d3f84a1dd00844192b8947b3
Kwayu Mmari <mmaridenis@gmail.com>
2023-05-08 00:11:42 +0300

    fixed bus service

--- lib/src/service/bus-services.dart
+++ lib/src/service/bus-services.dart
@@ -1,14 +1,11 @@
-import 'package:daladala_smart/src/provider/login-provider.dart';
-import 'package:provider/provider.dart';
+import 'package:daladala_smart/src/api/apis.dart';
 import 'package:flutter/material.dart';
-import '../api/apis.dart';
 
 class busService {
   Api api = Api();
 
   Future<void> bus(
       BuildContext context, String destination, String direction, String route) async {
-    final myProvider = Provider.of<MyProvider>(context, listen: false);
     Map<String, dynamic> data = {
       'destination': destination.toString(),
       'direction': direction.toString(),
