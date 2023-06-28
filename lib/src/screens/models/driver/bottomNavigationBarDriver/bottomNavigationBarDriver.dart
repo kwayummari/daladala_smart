efb299d0b9339d1a3a0b5809e951c02207dcd0da
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-29 02:15:38 +0300

    fixed driver profile

--- lib/src/screens/models/driver/bottomNavigationBarDriver/bottomNavigationBarDriver.dart
+++ lib/src/screens/models/driver/bottomNavigationBarDriver/bottomNavigationBarDriver.dart
@@ -1,8 +1,6 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, prefer_typing_uninitialized_variables, avoid_print, curly_braces_in_flow_control_structures, depend_on_referenced_packages, library_private_types_in_public_api, import_of_legacy_library_into_null_safe
-
-import 'package:daladala_smart/src/screens/models/client/dashboard/dashboard.dart';
-import 'package:daladala_smart/src/screens/models/client/profile/profile.dart';
-import 'package:daladala_smart/src/screens/models/client/rides/rides.dart';
+import 'package:daladala_smart/src/screens/models/driver/profile/profile.dart';
+import 'package:daladala_smart/src/screens/models/driver/rides/rides.dart';
 import 'package:daladala_smart/src/utils/app_const.dart';
 import 'package:flutter/material.dart';
 
@@ -17,7 +15,6 @@ class _bottomNavigationState extends State<bottomNavigation> {
   int index = 1;
   final Screen = [
     rides(),
-    dashboard(),
     profile(),
   ];
 
@@ -35,9 +32,6 @@ class _bottomNavigationState extends State<bottomNavigation> {
               data: Theme.of(context).copyWith(
                 canvasColor: AppConst.black,
                 primaryColor: AppConst.black,
-                // textTheme: Theme.of(context).textTheme.copyWith(
-                //     bodySmall:
-                //         GoogleFonts.vesperLibre(color: HexColor('#cbdd33')))
               ),
               child: BottomNavigationBar(
                 selectedItemColor: AppConst.primary,
@@ -46,8 +40,6 @@ class _bottomNavigationState extends State<bottomNavigation> {
                 items: [
                   BottomNavigationBarItem(
                       icon: Icon(Icons.car_rental), label: 'My Rides'),
-                  BottomNavigationBarItem(
-                      icon: Icon(Icons.home), label: 'Home'),
                   BottomNavigationBarItem(
                       icon: Icon(Icons.person_search_sharp), label: 'Profile'),
                 ],
