fba5b3d7154921c21da4f0162e4720d9bb277017
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-29 00:55:30 +0300

    fixed app_rich_text

--- lib/src/widgets/app_base_dropdown.dart
+++ lib/src/widgets/app_base_dropdown.dart
@@ -1,28 +1,27 @@
+import 'package:daladala_smart/src/widgets/app_image_network.dart';
 import 'package:daladala_smart/src/widgets/app_rich_text.dart';
 import 'package:daladala_smart/src/widgets/app_text.dart';
 import 'package:flutter/material.dart';
 
 class PatientResultsCard extends StatefulWidget {
-  final String client;
-  final String doctor;
-  final String blood;
-  final String pain;
-  final String smell;
-  final String fever;
-  final String questionare;
-  final String procedure;
-  final String timeline;
+  final String busNumber;
+  final String driverName;
+  final String conductorName;
+  final String numberOfSeats;
+  final String busType;
+  final String busOwner;
+  final String time;
+  final String image;
 
   const PatientResultsCard({
-    required this.client,
-    required this.doctor,
-    required this.blood,
-    required this.pain,
-    required this.smell,
-    required this.fever,
-    required this.questionare,
-    required this.procedure,
-    required this.timeline,
+    required this.busNumber,
+    required this.driverName,
+    required this.conductorName,
+    required this.numberOfSeats,
+    required this.busType,
+    required this.busOwner,
+    required this.time,
+    required this.image,
   });
 
   @override
@@ -34,7 +33,7 @@ class _PatientResultsCardState extends State<PatientResultsCard> {
 
   @override
   Widget build(BuildContext context) {
-    return widget.procedure == 'Procedure - Medical'
+    return widget.image == 'image - Medical'
         ? AnimatedContainer(
             duration: const Duration(milliseconds: 500),
             curve: Curves.easeInOut,
@@ -60,12 +59,7 @@ class _PatientResultsCardState extends State<PatientResultsCard> {
                   Row(
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      AppText(
-                        txt: widget.timeline +
-                            'hrs after ${widget.procedure == 'Procedure - Surgical' ? 'Surgical procedure' : 'Medical Procedure'}',
-                        size: 20,
-                        weight: FontWeight.bold,
-                      ),
+                      appNetworkImage(endPoint: 'bus/images/${widget.image}'),
                       IconButton(
                         icon: isExpanded
                             ? const Icon(Icons.keyboard_arrow_up)
@@ -84,132 +78,10 @@ class _PatientResultsCardState extends State<PatientResultsCard> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        if (widget.questionare == '-')
-                          richText(
-                            header: 'Emaotional Status: ',
-                            trailer:
-                                'We cannot determine your emotional status since it not yet 7 hours after ${widget.procedure}',
-                          )
-                        else if (int.parse(widget.questionare) >= 10)
-                          richText(
-                            header: 'Emaotional Status: ',
-                            trailer: 'Possible Depression',
-                          )
-                        else
-                          richText(
-                            header: 'Emaotional Status: ',
-                            trailer: 'You are fine',
-                          ),
-                        if (int.parse(widget.timeline) < 7 &&
-                            widget.pain == 'Worst Pain Possible')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          ),
-                        if (int.parse(widget.timeline) == 6 &&
-                            widget.pain == 'Severe')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) == 12 &&
-                            (widget.pain == 'Severe' ||
-                                widget.pain == 'Moderate'))
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else if ((int.parse(widget.timeline) <= 48 ||
-                                int.parse(widget.timeline) >= 24) &&
-                            widget.pain == 'Mild')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) >= 168 &&
-                            widget.pain == 'No Pain')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain}, please contact your health care provider',
-                          ),
-                        if (int.parse(widget.timeline) == 12 &&
-                            widget.blood == '3-4')
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) == 24 &&
-                            widget.blood == '3-4')
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) == 168 &&
-                            widget.blood == '2-3')
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) == 4018 &&
-                            widget.blood == '1-2')
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) > 4018 &&
-                            widget.blood == '0')
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads please contact your health care provider',
-                          ),
-                        if (int.parse(widget.timeline) >= 0 &&
-                            widget.fever == 'No')
-                          richText(
-                            header: 'Fever: ',
-                            trailer:
-                                'You have ${widget.fever} fever  which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Fever: ',
-                            trailer:
-                                'You have ${widget.fever} fever  please contact your health care provider',
-                          ),
-                        if (int.parse(widget.timeline) >= 0 &&
-                            widget.smell == 'No')
-                          richText(
-                            header: 'Smell: ',
-                            trailer:
-                                'You have ${widget.smell} Foul smell Vaginal Discharge  which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Smell: ',
-                            trailer:
-                                'You have ${widget.smell} Foul smell Vaginal Discharge please contact your health care provider',
-                          )
+                        richText(
+                          header: 'Plate Number: ',
+                          trailer: '${widget.busNumber}',
+                        )
                       ],
                     ),
                   ),
@@ -243,8 +115,7 @@ class _PatientResultsCardState extends State<PatientResultsCard> {
                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
                       AppText(
-                        txt: widget.timeline +
-                            'hrs after ${widget.procedure == 'Procedure - Surgical' ? 'Surgical procedure' : 'Medical Procedure'}',
+                        txt: 'Owner: ${widget.busOwner}',
                         size: 20,
                         weight: FontWeight.bold,
                       ),
@@ -266,101 +137,11 @@ class _PatientResultsCardState extends State<PatientResultsCard> {
                     child: Column(
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
-                        if (widget.questionare == '-')
-                          richText(
-                            header: 'Emaotional Status: ',
-                            trailer:
-                                'We cannot determine your emotional status since it not yet 7 hours after ${widget.procedure}',
-                          )
-                        else if (int.parse(widget.questionare) >= 10)
-                          richText(
-                            header: 'Emaotional Status: ',
-                            trailer: 'Possible Depression',
-                          )
-                        else
-                          richText(
-                            header: 'Emaotional Status: ',
-                            trailer: 'You are fine',
-                          ),
-                        if (int.parse(widget.timeline) < 24)
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel very sever pain which is normal for now',
-                          ),
-                        if (int.parse(widget.timeline) < 24 &&
-                            widget.pain == 'Very Severe')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) == 24 &&
-                            widget.pain == 'Mild')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) >= 48 &&
-                            widget.pain == 'No Pain')
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain} pain which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Pain: ',
-                            trailer:
-                                'You feel ${widget.pain}, please contact your health care provider',
-                          ),
-                        if (int.parse(widget.timeline) <= 72 &&
-                            widget.blood == '1-2')
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else if (int.parse(widget.timeline) >= 168 &&
-                            int.parse(widget.blood) == 0)
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} pads  which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Blood: ',
-                            trailer:
-                                'You used ${widget.blood} than 2 pads please contact your health care provider',
-                          ),
-                        if (int.parse(widget.timeline) >= 0 &&
-                            widget.fever == 'No')
-                          richText(
-                            header: 'Fever: ',
-                            trailer:
-                                'You have ${widget.fever} fever  which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Fever: ',
-                            trailer:
-                                'You have ${widget.fever} fever  please contact your health care provider',
-                          ),
-                        if (int.parse(widget.timeline) >= 0 &&
-                            widget.smell == 'No')
-                          richText(
-                            header: 'Smell: ',
-                            trailer:
-                                'You have ${widget.smell} Foul smell Vaginal Discharge  which is normal for now',
-                          )
-                        else
-                          richText(
-                            header: 'Smell: ',
-                            trailer:
-                                'You have ${widget.smell} Foul smell Vaginal Discharge please contact your health care provider',
-                          )
+                        richText(
+                          header: 'Bus Driver: ',
+                          trailer:
+                              '${widget.driverName}',
+                        )
                       ],
                     ),
                   ),
