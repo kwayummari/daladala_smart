b4a30b659a730611260640eb40120921a3ea432c
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-29 01:18:55 +0300

    fixed app_rich_text

--- lib/src/screens/models/client/rides/myRides.dart
+++ lib/src/screens/models/client/rides/myRides.dart
@@ -1,3 +1,4 @@
+import 'package:daladala_smart/src/service/bus-services.dart';
 import 'package:daladala_smart/src/widgets/app_base_screen.dart';
 import 'package:flutter/material.dart';
 
@@ -9,6 +10,20 @@ class myRides extends StatefulWidget {
 }
 
 class _myRidesState extends State<myRides> {
+  late Future<List> _futureBusList;
+
+  @override
+  void initState() {
+    super.initState();
+    _futureBusList = getBuses();
+  }
+
+  Future<List> getBuses() async {
+    final busService _busesService = await busService();
+    final List busesList = await _busesService.getBusById(
+        context);
+    return busesList;
+  }
   @override
   Widget build(BuildContext context) {
     return AppBaseScreen(
--- lib/src/service/bus-services.dart
+++ lib/src/service/bus-services.dart
@@ -1,4 +1,5 @@
 import 'package:daladala_smart/src/api/apis.dart';
+import 'package:daladala_smart/src/functions/splash.dart';
 import 'package:flutter/material.dart';
 
 class busService {
@@ -10,6 +11,16 @@ class busService {
       'destination': destination.toString(),
       'direction': direction.toString(),
       'route': route.toString(),
+    };
+    final response = await api.post(context, 'bus/get.php', data);
+    return response;
+  }
+  Future getBusById(
+      BuildContext context) async {
+        final SplashFunction _splashService = await SplashFunction();
+    final String id = await _splashService.getId();
+    Map<String, dynamic> data = {
+      'id': id.toString(),
     };
     final response = await api.post(context, 'bus/get.php', data);
     return response;
