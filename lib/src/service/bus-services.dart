3f974152e5055870e2c39ee28acc5e2036bffc90
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-29 02:54:44 +0300

    fixed driver profile

--- lib/src/service/bus-services.dart
+++ lib/src/service/bus-services.dart
@@ -25,13 +25,17 @@ class busService {
     final response = await api.post(context, 'bookings/get_by_id.php', data);
     return response;
   }
+
   Future getBusByDriverId(BuildContext context) async {
     final SplashFunction _splashService = await SplashFunction();
     final String id = await _splashService.getId();
     Map<String, dynamic> data = {
       'id': id.toString(),
     };
-    final response = await api.post(context, 'bookings/get_by_driver_id.php', data);
-    return response;
+    final response =
+        await api.post(context, 'bookings/get_by_driver_id.php', data);
+    final responses =
+        await api.post(context, 'bookings/get_by_conductor_id.php', data);
+    return response == null ? responses : response;
   }
 }
