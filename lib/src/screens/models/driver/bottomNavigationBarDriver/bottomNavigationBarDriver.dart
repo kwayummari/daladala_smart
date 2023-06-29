a68239c4a5683b3089511d22b03e5a811134b0ec
Kwayu Mmari <mmaridenis@gmail.com>
2023-06-29 03:10:42 +0300

    fixed driver profile

--- lib/src/screens/models/driver/bottomNavigationBarDriver/bottomNavigationBarDriver.dart
+++ lib/src/screens/models/driver/bottomNavigationBarDriver/bottomNavigationBarDriver.dart
@@ -1,5 +1,6 @@
 // ignore_for_file: file_names, prefer_const_constructors, prefer_const_literals_to_create_immutables, non_constant_identifier_names, prefer_typing_uninitialized_variables, avoid_print, curly_braces_in_flow_control_structures, depend_on_referenced_packages, library_private_types_in_public_api, import_of_legacy_library_into_null_safe
 import 'package:daladala_smart/src/screens/models/driver/profile/profile.dart';
+import 'package:daladala_smart/src/screens/models/driver/rides/myRides.dart';
 import 'package:daladala_smart/src/screens/models/driver/rides/rides.dart';
 import 'package:daladala_smart/src/utils/app_const.dart';
 import 'package:flutter/material.dart';
@@ -14,7 +15,7 @@ class bottomNavigationDriver extends StatefulWidget {
 class _bottomNavigationDriverState extends State<bottomNavigationDriver> {
   int index = 0;
   final Screen = [
-    rides(),
+    driverMyRides(),
     profile(),
   ];
 
